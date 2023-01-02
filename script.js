b4a2cf9ca28ef91f7b65c861f0df641d87ddf5b5
himansh2203 <2203himanshusingh@gmail.com>
2023-01-02 20:57:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// showing window alert on clicking the button
+const btn=document.getElementById("btn");
+
+btn.addEventListener("click" , function(){
+    window.alert("efghe");
+})
+
+// image change on ckick
